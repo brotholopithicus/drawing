8a52754df7524915f3cef6f8f593d5f2b7aac4cc
James Jarvis <jamespjarvis@gmail.com>
2017-06-01 17:45:05 -0400

    added white

--- public/js/combo.js
+++ public/js/combo.js
@@ -3,7 +3,7 @@ function App() {
     colors: [
       '#001F3F', '#0074D9', '#7FDBFF',
       '#39CCCC', '#3D9970', '#2ECC40',
-      '#01FF70', '#FFDC00', '#FF851B',
+      '#01FF70', '#FFDC00', '#FF851B', '#FFFFFF',
       '#FF4136', '#F012BE', '#B10DC9',
       '#85144B', '#AAAAAA', '#111111',
     ]
